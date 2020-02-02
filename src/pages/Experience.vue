5067a76e49ddf8d3d373d6f07449552271eb8cf1
webdevsuperfast <acob.rotsen@hotmail.com>
2020-02-02 20:25:50 +0800

    Updated content

--- src/pages/Experience.vue
+++ src/pages/Experience.vue
@@ -1,12 +1,33 @@
 <template>
   <Layout sidebar="true" :title="pageTitle">
     <Main :sectionID="pageTitle | slugify" :sectionClass="pageTitle | slugify">
-      <div 
-        v-for="{ node } in $page.allExperience.edges" 
-        :key="node.id"
-        :class="['experience', 'w-100', `experience-${node.id}`]"
-      >
-        
+      <div class="w-100">
+        <div
+          class="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
+          v-for="({node}, index) in $page.allExperience.edges"
+          :key="index"
+        >
+          <div class="resume-content">
+            <h4 class="mb-0">{{ node.position }}, <span class="text-muted">{{ node.company }}</span></h4>
+            <!-- <div class="subheading mb-0 text-muted">{{ node.company }}</div> -->
+            <div class="resume-date mb-3">
+              <p
+                class="text-muted"
+                v-for="(date, index) in node.date"
+                :key="index"
+              >{{ date.start }} - {{ date.end }}</p>
+            </div>
+            <ul class="mb-0 summary-list">
+              <li
+                v-for="(summary, key) in node.summary"
+                :key="key"
+              >
+                <check-circle-icon size="1x" class="mr-1" />
+                {{ summary }}
+              </li>
+            </ul>
+          </div>
+        </div>
       </div>
     </Main>
   </Layout>
@@ -14,7 +35,7 @@
 
 <page-query>
 {
-  allExperience {
+  allExperience(sortBy: "date", order: ASC) {
     edges {
       node {
         id,
@@ -32,22 +53,41 @@
 </page-query>
 
 <script>
-import { CodeIcon } from "vue-feather-icons";
+import {
+  CheckCircleIcon
+} from "vue-feather-icons";
 
 import Main from "@/layouts/Main";
 
 export default {
+  metaInfo: {
+    title: "Experience"
+  },
   data() {
     return {
       pageTitle: "Experience"
-    }
-  },
-  metaInfo: {
-    title: "My Work Experience"
+    };
   },
   components: {
-    CodeIcon,
-    Main
+    Main,
+    CheckCircleIcon
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+  ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+    li {
+      display: block;
+      padding-left: 0;
+    }
+  }
+  h4 {
+    span {
+      font-weight: 400;
+    }
+  }
+</style>
