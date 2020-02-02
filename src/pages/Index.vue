c7f9f122e35d3fa1a7a7a42b1ae65cc197265911
webdevsuperfast <acob.rotsen@hotmail.com>
2020-02-02 20:26:24 +0800

    Updated content

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,18 +1,36 @@
 <template>
   <Layout sidebar="true" :title="pageTitle">
     <Main :sectionID="pageTitle | slugify" :sectionClass="pageTitle | slugify">
-      <!-- <div class="w-100">
-        <div class="subheading text-uppercase text-secondary">I'm Rotsen Mark</div>
-        <transition name="fade">
-          <h1 class="headlines">
-            Web & WordPress Developer
-          </h1>
-        </transition>
+      <div class="w-100">
+        <p
+          class="lead"
+        >I’m a web developer, a self-proclaimed introvert and a *NIX enthusiast.</p>
+        <b-dropdown split text="Learn More" class="social-links">
+          <b-dropdown-item v-for="{node} in $page.allSocial.edges" :key="node.id" :href="node.url" target="_blank">
+            {{ node.name }}
+          </b-dropdown-item>
+        </b-dropdown>
         <b-button variant="outline-success" to="/contact">Hire Me</b-button>
-      </div> -->
+      </div>
     </Main>
   </Layout>
 </template>
+
+<page-query>
+{
+  allSocial {
+    edges {
+      node {
+        id,
+        name,
+        url,
+        title,
+        icon
+      }
+    }
+  }
+}
+</page-query>
 
 <script>
 import Aside from "@/layouts/Aside";
