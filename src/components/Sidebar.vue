8bbe41bc39d1489a9c2564d1c2ccd6991a3e6254
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-02-02 10:12:18 +0800

    Updated markup, use rotating headlines on mobile

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -2,12 +2,15 @@
   <div class="sidebar d-flex align-items-center text-center m-auto">
     <div class="w-100">
       <div class="logo m-auto" v-if="title == 'Home'">
-        <g-image class="img-fluid mb-0" src="~/images/logo.svg" alt="Rotsen Mark Acob" />
-        <h1 class="text-light mb-0">Rotsen Mark Acob</h1>
-        <div class="position text-light text-uppercase">
+        <g-image class="img-fluid mb-0 mb-4 mb-sm-0" src="~/images/logo.svg" alt="Rotsen Mark Acob" />
+        <h1 class="text-light mb-0 d-none d-sm-block">Rotsen Mark Acob</h1>
+        <div class="position text-light text-uppercase d-none d-sm-block">
           <span class="position-name">Web Developer</span>
           <span class="position-name">WordPress Developer</span>
           <span class="position-name">Freelancer</span>
+        </div>
+        <div class="position text-light text-uppercase headlines d-block d-sm-none">
+          <span v-html="headlines[index]"></span>
         </div>
       </div>
       <h1 class="page-title" v-else>{{ title }}</h1>
@@ -20,6 +23,33 @@ export default {
   props: {
     title: String
   },
-  components: {}
+  components: {},
+  data() {
+    return {
+      index: 0,
+      rotated: false,
+      headlines: [
+        'Web Developer',
+        'WordPress Developer',
+        'Frontend Developer',
+        'Freelancer'
+      ]
+    }
+  },
+  methods: {
+    rotateText: function() {
+      setTimeout(() => {
+        this.index += 1;
+        if ( this.index >= this.headlines.length) {
+          this.index = 0;
+        }
+        this.rotateText();
+      }, 3000);
+    }
+  },
+  mounted() {
+    this.rotateText();
+    this.rotated = true;
+  }
 };
 </script>
