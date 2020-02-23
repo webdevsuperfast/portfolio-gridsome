c2a5b3f3e877eafca207ba7b89d04417cb59a738
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-02-24 07:42:54 +0800

    Updated layout

--- src/pages/Testimonials.vue
+++ src/pages/Testimonials.vue
@@ -3,24 +3,26 @@
     <Main :sectionClass="pageTitle | slugify" :sectionID="pageTitle | slugify">
       <div class="w-100">
         <h2>What Clients Say About Me</h2>
-        <div
-          v-for="({ node }, index)  in $page.allTestimonials.edges"
-          :key="index"
-          :class="['testimonial', `testimonial-${node.id}`]"
-        >
-          <b-media :right-align="index % 2 ? true : false" class="testimonial-image">
-            <template v-slot:aside>
-              <g-image class="img-fluid portfolio-image" src="~/images/upwork.png" width="100" height="100" />
-            </template>
+        <b-row>
+          <b-col cols="12" sm="6"
+            v-for="({ node }, index)  in $page.allTestimonials.edges"
+            :key="index"
+            :class="['testimonial', `testimonial-${node.id}`]"
+          >
             <div class="testimonial-content mb-2">
               <blockquote v-html="node.review" class="mb-0"></blockquote>
             </div>
-            <div class="author-information">
-              <h4 class="testimonial-title mb-0">{{ node.name }}</h4>
-              <p class="small text-secondary">{{ node.location }}</p>
-            </div>
-          </b-media>
-        </div>
+            <b-media class="testimonial-image">
+              <template v-slot:aside>
+                <g-image class="img-fluid portfolio-image" src="~/images/upwork.png" width="60" height="60" />
+              </template>
+              <div class="author-information">
+                <h4 class="testimonial-title mb-0">{{ node.name }}</h4>
+                <p class="small text-secondary">{{ node.location }}</p>
+              </div>
+            </b-media>
+          </b-col>
+        </b-row>
       </div>
     </Main>
   </Layout>
