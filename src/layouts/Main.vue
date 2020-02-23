21b310f2b7d5e667f1aa589de0def8096a3ebe1c
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-02-24 07:40:46 +0800

    Updated layout

--- src/layouts/Main.vue
+++ src/layouts/Main.vue
@@ -1,9 +1,7 @@
 <template>
-  <b-container class="p-0" fluid>
-    <section :id="`section-${sectionID}`" :class="['resume-section', 'd-flex', 'align-items-center', `section-${sectionClass}`]">
+  <section :id="`section-${sectionID}`" :class="['resume-section', 'd-flex', 'align-items-center', `section-${sectionClass}`]">
       <slot />
-    </section>
-  </b-container>
+  </section>
 </template>
 <script>
 export default {
