9938f4980147ea64b7aff5696b8beb9cfe654693
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2020-02-25 16:22:35 +0800

    Removed unneeded components and markup

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,22 +1,9 @@
 <template>
   <div class="site-inner">
-    <div id="sideNav" class="p-0 container-fluid" v-if="sidebar">
-      <Sidebar :title="title" />
-    </div>
     <slot />
   </div>
 </template>
 
 <script>
-import Sidebar from "@/components/Sidebar";
-
-export default {
-  components: {
-    Sidebar
-  },
-  props: [
-    'sidebar',
-    'title'
-  ]
-}
+export default {}
 </script>
