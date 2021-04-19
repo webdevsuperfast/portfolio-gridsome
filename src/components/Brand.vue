12f428dd31dfac4df1eccdfbcf4480cab7fd75c6
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2021-04-20 07:34:39 +0800

    Moved logo to new component

---
+++ src/components/Brand.vue
@@ -0,0 +1,22 @@
+<template>
+  <b-navbar-brand to="#app">
+    <span class="d-block d-lg-none">{{ $static.metadata.siteName }}</span>
+    <span class="d-none d-lg-block">
+      <b-img-lazy class="img-fluid img-profile rounded-circle mx-auto mb-2" src="~/images/logo.svg" :alt="$static.metadata.siteName" />
+    </span>
+  </b-navbar-brand>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+  
+};
+</script>
