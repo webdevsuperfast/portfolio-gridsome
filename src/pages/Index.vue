7f58ea19c870bd61ea2a3f4cfad6c60e844ef5d7
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2021-04-22 11:38:26 +0800

    Added social media links

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,6 +3,11 @@
     <Main :sectionID="pageTitle | slugify" :sectionClass="pageTitle | slugify">
       <div class="w-100">
         <p class="lead" v-html="$page.wordPressPage.content" />
+        <b-dropdown split text="Follow Me" class="social-links">
+          <b-dropdown-item v-for="{ social, index } in $page.wordPressPage.acf.socialMedia" :key="index" :href="social.url" target="_blank">
+            {{ social.name }}
+          </b-dropdown-item>
+        </b-dropdown>
         <b-button variant="outline-success" to="/contact">Contact</b-button>
       </div>
     </Main>
@@ -14,7 +19,15 @@
   wordPressPage(id: 439) {
     id,
     title,
-    content
+    content,
+    acf {
+      socialMedia {
+        social {
+          name,
+          url
+        }
+      }
+    }
   }
 }
 </page-query>
