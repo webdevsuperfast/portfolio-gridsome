5437a2b43334bbc258cfa23f55223bdbc900a4cd
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-29 10:32:32 +0800

    Edited tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,31 +1,35 @@
 module.exports = {
+  mode: "jit",
   content: [
-    './src/**/*.{vue,js,md,html}',
-    './src/components/*.vue',
-    './src/layouts/*.vue',
-    './src/pages/*.vue'
+    "./src/**/*.{vue,js,md,html}",
+    "./src/components/*.vue",
+    "./src/layouts/*.vue",
+    "./src/pages/*.vue",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        body: 'url(../static/images/background.jpg)',
+        body: "url(../static/images/background.jpg)",
       },
       fontFamily: {
-        barlow: ['Barlow', 'sans-serif'],
-        barlowCondensed: ['Barlow Condensed', 'sans-serif'],
+        barlow: ["Barlow", "sans-serif"],
+        "barlow-condensed": ["Barlow Condensed", "sans-serif"],
       },
       fontSize: {
-        '14xl': ['14rem', {
-          lineHeight: '1'
-        }],
-        '16xl': ['16rem', {
-          lineHeight: '1'
-        }]
+        "14xl": [
+          "14rem",
+          {
+            lineHeight: "1",
+          },
+        ],
+        "16xl": [
+          "16rem",
+          {
+            lineHeight: "1",
+          },
+        ],
       },
     },
   },
-  plugins: [
-    require('@tailwindcss/forms'),
-    require('@tailwindcss/typography')
-  ],
-}
+  plugins: [require("@tailwindcss/forms"), require("@tailwindcss/typography")],
+};
