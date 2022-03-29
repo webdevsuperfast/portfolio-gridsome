0cc00f5c59ae3944ab7eb52d43a474eafb3ccbeb
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-29 10:34:01 +0800

    Code cleanup, added social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,11 +1,12 @@
 <template>
-  <footer class="pb-4 lg:pb-24 w-full fixed bottom-0">
-    <div class="container mx-auto px-4 sm:px-6 lg:px-8">
+  <footer class="fixed bottom-0 w-full pb-4 lg:pb-24">
+    <div class="container px-4 mx-auto sm:px-6 lg:px-8">
       <div class="relative">
         <button
-          @click="socialNav = ! socialNav" 
-          class="p-5 bg-rose-900 rounded-xl rounded-br-none absolute right-0 bottom-0 hover:bg-rose-700 active:bg-rose-700 focus:bg-rose-700">
-          <div class="w-5 h-5 flex items-center justify-center relative">
+          @click="socialNav = !socialNav"
+          class="absolute bottom-0 right-0 p-5 rounded-br-none bg-rose-900 rounded-xl hover:bg-rose-700 active:bg-rose-700 focus:bg-rose-700"
+        >
+          <div class="relative flex items-center justify-center w-5 h-5">
             <span
               :class="socialNav ? 'rotate-45' : ''"
               class="transform transition w-full h-0.5 absolute rounded bg-white"
@@ -13,37 +14,50 @@
             <span
               :class="socialNav ? 'rotate-45' : ''"
               class="transform transition h-full absolute w-0.5 rounded bg-white"
-              ></span>
+            ></span>
           </div>
         </button>
         <div
-          :class="{ 'block': socialNav, 'hidden': ! socialNav }" 
-          class="absolute right-0 bottom-20 rounded-xl bg-rose-900 overflow-hidden"
+          :class="{ block: socialNav, hidden: !socialNav }"
+          class="absolute right-0 overflow-hidden bottom-20 rounded-xl bg-rose-900"
         >
           <div class="social-navigation">
-            <a class="text-white uppercase block text-center hover:text-gray-200 hover:bg-rose-700 transition-all py-3 px-5 lg:py-5 lg:px-10 lg:text-xl" href="https://github.com/webdevsuperfast" target="_blank">
+            <a
+              class="block px-5 py-3 text-center text-white uppercase transition-all hover:text-gray-200 hover:bg-rose-700 lg:py-5 lg:px-10 lg:text-xl"
+              href="https://github.com/webdevsuperfast"
+              target="_blank"
+            >
               GitHub
             </a>
-            <a class="text-white uppercase block text-center hover:text-gray-200 hover:bg-rose-700 transition-all py-3 px-5 lg:py-5 lg:px-10 lg:text-xl" href="" target="_blank">
+            <a
+              class="block px-5 py-3 text-center text-white uppercase transition-all hover:text-gray-200 hover:bg-rose-700 lg:py-5 lg:px-10 lg:text-xl"
+              href="https://www.linkedin.com/in/acobrotsen"
+              target="_blank"
+            >
               LinkedIn
             </a>
-            <a class="text-white uppercase block text-center hover:text-gray-200 hover:bg-rose-700 transition-all py-3 px-5 lg:py-5 lg:px-10 lg:text-xl" href="" target="_blank">
+            <a
+              class="block px-5 py-3 text-center text-white uppercase transition-all hover:text-gray-200 hover:bg-rose-700 lg:py-5 lg:px-10 lg:text-xl"
+              href="https://profiles.wordpress.org/frodobean"
+              target="_blank"
+            >
               WordPress
             </a>
           </div>
         </div>
       </div>
     </div>
-  </footer><!-- End Footer -->
+  </footer>
+  <!-- End Footer -->
 </template>
 
 <script>
 export default {
-  name: 'Footer',
+  name: "Footer",
   data() {
     return {
-      socialNav: false
-    }
-  } 
-}
+      socialNav: false,
+    };
+  },
+};
 </script>
