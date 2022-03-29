56b76d22fb76f1d445f7e0c4a68b1f0403da77ea
Rotsen Mark Acob <acob.rotsen@hotmail.com>
2022-03-29 14:16:29 +0800

    Code cleanup

--- src/pages/Thanks.vue
+++ src/pages/Thanks.vue
@@ -1,27 +1,23 @@
 <template>
-  <Layout sidebar="true" :title="pageTitle">
-    <Main :sectionID="pageTitle | slugify" :sectionClass="pageTitle | slugify">
-      <div class="w-100">
-        <h2 class="mb-2">Thank you for contacting me!</h2>
-        <p class="lead">I'll be getting back to you, shortly.</p>
-        <b-button variant="outline-primary" to="/">Return to Homepage</b-button>
-      </div>
-    </Main>
+  <Layout>
+    <div class="w-100">
+      <h2 class="mb-2">Thank you for contacting me!</h2>
+      <p class="lead">I'll be getting back to you, shortly.</p>
+      <b-button variant="outline-primary" to="/">Return to Homepage</b-button>
+    </div>
   </Layout>
 </template>
 
 <script>
 export default {
   metaInfo: {
-    title: "Thank You"
+    title: "Thank You",
   },
   data() {
     return {
-      pageTitle: "Thank You"
-    }
+      pageTitle: "Thank You",
+    };
   },
-  components: {
-    
-  }
-}
+  components: {},
+};
 </script>
